3d50b74df3628257c0d4fe3be9387264644598f9
butterbastedsteak <bharathlivingston@gmail.com>
2026-07-20 21:45:33 +0530

    first commit

--- equalityCheck.c
+++ equalityCheck.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-int main()
-
-{
+int main() {
   int a, b;
-
   printf("enter a : ");
   scanf("%d", &a);
   printf("enter b : ");
   scanf("%d", &b);
   if (a == b) {
     printf("a and b are equal\n");
-
   } else {
     printf("a and b are not equal\n");
   }
